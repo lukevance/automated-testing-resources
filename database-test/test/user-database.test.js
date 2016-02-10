fa560eb64be3b6a13ec0cdd8be010ed13dbf6e60
Luke Vance <lukevance90@gmail.com>
2016-02-09 23:10:48 -0700

    finished assignment for database testing

--- database-test/test/user-database.test.js
+++ database-test/test/user-database.test.js
@@ -1,17 +1,17 @@
 var chai = require('chai');
 var user = require('../user-database.js');
-var knex = require('../db/knex')
+var knex = require('../db/knex');
 
 var expect = chai.expect;
 
 describe('Test user database functionality', function(){
-   
+
     before(function(done) {
-        console.log('before')
+        console.log('before');
         knex.migrate.rollback();
         done();
     });
-    
+
     beforeEach(function(done) {
         return knex.migrate.latest()
         .then(function() {
@@ -19,16 +19,16 @@ describe('Test user database functionality', function(){
             done();
         });
     });
-    
-    afterEach(function(done) {  
+
+    afterEach(function(done) {
         return knex.migrate.rollback()
         .then(function() {
             console.log('afterEach');
             done();
         });
     });
-   
-    
+
+
    it('should insert a user into database', function(done){
         user.add('roger.schmidt','roger','schmdit').then(
             function(data){
@@ -36,19 +36,69 @@ describe('Test user database functionality', function(){
                 done();
             },
             function(err){
-                expect.fail('failed insert');
-                done();
+                expect.fail('failed insert ' + err);
+                done(err);
             }
-        )
+        );
    });
-    
-   it('should get a list of all users');
-   
-   it('should get a specific user with a give userId');
-   
-   it('should delete a specific user with a given userId')
-   
-   it('should update a specific user with a given userId and updated information')
-    
-});
 
+   it('should get a list of all users', function(done){
+     user.add('luke.vance', 'luke', 'vance').then(
+       function(){
+         user.add('roger.schmidt','roger','schmdit').then(
+           function(){
+             user.getAll()
+              .then(
+               function(data){
+                 expect(data.length).to.equal(2);
+                 done();
+               },
+               function(err){
+                 expect.fail('failed getAll ' + err);
+                 done(err);
+               });
+           });
+       });
+   });
+
+   it('should get a specific user with a give userId', function(done){
+     user.add('luke.vance', 'luke', 'vance').then(
+       function(){
+         user.getOne(1).then(
+           function(data){
+             expect(data[0].userId).to.equal(1);
+             done();
+           }, function(err) {
+             expect.fail('failed getOne ' + err);
+           });
+       });
+   });
+
+   it('should delete a specific user with a given userId', function(done){
+     user.add('luke.vance', 'luke', 'vance').then(
+       function(){
+         user.deleteOne(1).then(
+           function(data){
+             expect(data).to.equal(1);
+             done();
+           }, function(err){
+             console.log(err);
+             done(err);
+           });
+       });
+   });
+
+   it('should update a specific user with a given userId and updated information', function(done){
+     user.add('luke.vance', 'luke', 'vance').then(
+      function(){
+        user.updateOne(1, {userName: 'provo149'}).then(
+          function(data){
+            expect(data).to.equal(1);
+            done();
+          }, function(err) {
+            console.log(err);
+            done(err);
+        });
+    });
+  });
+});
--- database-test/user-database.js
+++ database-test/user-database.js
@@ -2,7 +2,25 @@ var knex = require('./db/knex');
 
 module.exports = {
     add: function(userName, fName, lName){
-        return knex('users').insert({'userName': userName, 'fName': fName, 'lName': lName})
+        return knex('users').insert({'userName': userName, 'fName': fName, 'lName': lName});
+    },
+    getAll: function() {
+      return knex('users');
+    },
+    getOne: function(id) {
+      return knex('users').where({'userId': id});
+    },
+    updateOne: function(id, newVals) {
+      return knex('users').where({'userId': id}).update(newVals);
+    },
+    deleteOne: function(id) {
+      return knex('users').where({'userId':id}).del();
     }
-    
-}
+
+};
+
+
+// Get all users from database,
+// Get one user with a given id,
+// Update 1 user with a given id, and updated values,
+// Delete 1 user with a given id.
